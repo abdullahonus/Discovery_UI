fcfc67cdabe90d81bae53713c57a5b9e9c6d80a3
abdullahonus <abdullah.onus0@gmail.com>
2021-11-30 00:37:52 +0300

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:discovery_c4/discover_c4.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: DiscoverPage(),
+    );
+  }
+}
